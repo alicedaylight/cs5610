c32edb3c58d64da04d7bd36f9b5521c3f707c183
alicedaydream <xoxoumop3pisdn@Alices-MacBook-Air.local>
2017-07-24 10:27:10 -0700

    some database updates

--- server.js
+++ server.js
@@ -13,49 +13,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 var mongoose = require('mongoose');
 
-// var express = require('./express');
-
-// loads the express from the node modeules and not the native express
-
-
+var routeServices = require('./assignment/app');
+routeServices(app);
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname+'/public/assignment'));
 // remove 'public' so that the static will start from cs5610 directory and not public
 
-//model.server etc
-
-// module.exports = function() {
-// var connectionString = null;
-//
-// // heruku db
-// if (process.env.MONGODB_URI) {
-//     connectionString = "mongodb://<dbuser>:<dbpassword>@ds147551.mlab.com:47551/heroku_x5526pjm";
-// } else {
-//     // local db
-//     connectionString = 'mongodb://localhost:27017/cs5610';
-//     // "cs5610" is the name
-// }
-//
-// // mongoose is ontop of mongodb
-// // moongose provides api for CRUD operations
-// var mongoose = require('mongoose');
-// mongoose.connect(connectionString);
-//
-// var userMode = require("./user/user.model.server.js")(mongoose);
-//
-// var models = {
-//     'userModel' : userModel;
-
-
-
-// I NEED THIS BEL
 
 var connectionString = 'mongodb://localhost:27017/cs5610Webdev';
 mongoose.connect(connectionString);
 
-//var appFunction = require('./assignment/app');
-//appFunction(app);
 
 app.post('/api/createuser',function (req, res) {
     var user = req.body;
